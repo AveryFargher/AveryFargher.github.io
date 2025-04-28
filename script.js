dd233e55c5b326d584a8730375942e883ae32fd2
AveryFargher <averyfargher06@gmail.com>
2025-04-28 00:53:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ visibleSection.style.display = "block";
 sectionButtons.forEach((button, index) => {
   button.onclick = function(){
     visibleSection.style.display = "none";
-    visibleSection = sectionList[index];
+    visibleSection = sectionsList[index];
     visibleSection.style.display = "block";}
 });
