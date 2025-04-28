6ee8909b3f0f5cadab8c3ce61446e2087a995fcf
AveryFargher <averyfargher06@gmail.com>
2025-04-28 00:22:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 
-const sections = document.querySelectorAll('.section');
+const sectionsList = document.querySelectorAll('.section');
+const sections = document.getElementById("sections");
+
+
+
+
+for(const section of sectionsList){
+  let button = document.createElement("span");
+  button.textContent = section.id.replace("-"," ");
+  sections.appendChild(button);
+}
