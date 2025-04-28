f30107eeb61c7136fd323a0146332799c3819f5e
AveryFargher <averyfargher06@gmail.com>
2025-04-28 01:41:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,29 @@
+function setupTabs(listSelector, barId) {
+  const list = document.querySelectorAll(listSelector);
+  const bar = document.getElementById(barId);
+  let buttons = [];
 
-const sectionsList = document.querySelectorAll('.section');
-const sectionsBar = document.getElementById("sectionsBar");
-let sectionButtons = [];
+  list.forEach(item => {
+    let button = document.createElement("span");
+    button.textContent = item.id.replace(/-/g, " ");
+    bar.appendChild(button);
+    buttons.push(button);
+    item.style.display = "none";
+  });
 
+  let visibleItem = list[0];
+  visibleItem.style.display = "block";
 
-
-for(const section of sectionsList){
-  let button = document.createElement("span");
-  button.textContent = section.id.replace(/-/g," ");
-  sectionsBar.appendChild(button);
-  sectionButtons.push(button);
-  section.style.display = "none"
+  buttons.forEach((button, index) => {
+    button.onclick = function() {
+      visibleItem.style.display = "none";
+      visibleItem = list[index];
+      visibleItem.style.display = "block";
+    };
+  });
 }
 
-let visibleSection = sectionsList[0];
-visibleSection.style.display = "block";
-
-sectionButtons.forEach((button, index) => {
-  button.onclick = function(){
-    visibleSection.style.display = "none";
-    visibleSection = sectionsList[index];
-    visibleSection.style.display = "block";}
-});
-
-
-const projectsList = document.querySelectorAll('.project');
-const projectsBar = document.getElementById("projectsBar");
-let projectButtons = [];
-
-
-
-for(const project of projectsList){
-  let button = document.createElement("span");
-  button.textContent = project.id.replace(/-/g," ");
-  projectsBar.appendChild(button);
-  projectButtons.push(button);
-  project.style.display = "none"
-}
-
-let visibleProject = projectsList[0];
-visibleProject.style.display = "block";
-
-projectButtons.forEach((button, index) => {
-  button.onclick = function(){
-    visibleProject.style.display = "none";
-    visibleProject = projectsList[index];
-    visibleProject.style.display = "block";}
-});
-
-
-
-const interestsList = document.querySelectorAll('.interest');
-const interestsBar = document.getElementById("interestsBar");
-let interestButtons = [];
-
-
-
-for(const interest of interestsList){
-  let button = document.createElement("span");
-  button.textContent = interest.id.replace(/-/g," ");
-  interestsBar.appendChild(button);
-  interestButtons.push(button);
-  interest.style.display = "none"
-}
-
-let visibleInterest = interestsList[0];
-visibleInterest.style.display = "block";
-
-interestButtons.forEach((button, index) => {
-  button.onclick = function(){
-    visibleInterest.style.display = "none";
-    visibleInterest = interestsList[index];
-    visibleInterest.style.display = "block";}
-});
+// Set up each tab group
+setupTabs('.section', 'sectionsBar');
+setupTabs('.project', 'projectsBar');
+setupTabs('.interest', 'interestsBar');
